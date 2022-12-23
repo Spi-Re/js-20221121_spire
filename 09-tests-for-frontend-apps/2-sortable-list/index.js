f415951049ca7ca2a50e9cf5407774a388d4abd5
Ivan Burmistrov <vanya.burmistrov007@gmail.com>
2022-12-23 17:43:59 +0400

    some work on drogaable state

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -14,11 +14,67 @@ export default class SortableList {
     });
     wrapper.append(ul);
     this.element = wrapper.firstChild;
-    // document.body.append(this.element); // Уже есть в HTML
+    document.body.append(this.element);
     this.eventListeners();
   }
 
-  copyElem(className, innerHTML) {
+  copyTarget(className, innerHTML) {
+    const copyTarget = document.createElement("li");
+    copyTarget.className = className;
+    copyTarget.innerHTML = innerHTML;
+    copyTarget.style.position = "absolute";
+    // style.top работает до margin
+    copyTarget.style.margin = 0 + "px";
+    copyTarget.style.zIndex = 1000;
+    return copyTarget;
+  }
+
+  eventListeners() {
+    let copyOfTargetHTML = "";
+
+    this.element.addEventListener("pointerdown", (event) => {
+      event.preventDefault();
+      let target = null;
+      // TODO: Нужно достать координаты всех span, которые можно грабать
+      // TODO: При переводе на эти координаты, менять весь li местами с target
+      // YES = TODO: При pointerUp добавлять классы и HTML target и удалять копию
+
+      if (event.target.closest("[data-grab-handle]")) {
+        target = event.target.closest(".sortable-list__item");
+        target.ondragstart = () => false;
+        copyOfTargetHTML = target.innerHTML;
+
+        const copyTarget = this.copyTarget(target.className, copyOfTargetHTML);
+        document.body.append(copyTarget);
+        target.innerHTML = "";
+        target.classList.add("sortable-list__placeholder");
+
+        moveAt(event.pageY);
+        document.addEventListener("pointermove", onPointerMove);
+        copyTarget.addEventListener("pointerup", onPointerUp);
+
+        // ============================================================
+        function onPointerMove(event) {
+          moveAt(event.pageY);
+        }
+        function moveAt(pageY) {
+          copyTarget.style.width = target.offsetWidth + "px";
+          copyTarget.style.left = copyTarget.offsetLeft + "px";
+          copyTarget.style.top = pageY - target.offsetHeight / 2 + "px";
+        }
+        function onPointerUp() {
+          target.innerHTML = copyOfTargetHTML;
+          target.classList.remove("sortable-list__placeholder");
+          copyTarget.remove();
+          document.removeEventListener("pointermove", onPointerMove);
+          copyTarget.onpointerup = null;
+        }
+      }
+    });
+  }
+}
+/* 
+copyElem(className, innerHTML) {
     const copyTarget = document.createElement("li");
     copyTarget.className = className;
     copyTarget.innerHTML = innerHTML;
@@ -74,4 +130,4 @@ export default class SortableList {
       }
     });
   }
-}
+*/
