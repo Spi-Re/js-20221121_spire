647f78c2c65e0c1d4176eb8bb54da99ed03389ad
Ivan Burmistrov <vanya.burmistrov007@gmail.com>
2022-12-24 19:05:09 +0400

    made the drag-n-drop)

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -8,7 +8,7 @@ export default class SortableList {
     const wrapper = document.createElement("div");
     const ul = document.createElement("ul");
     ul.className = "sortable-list";
-    let i = 0;
+    let i = 1;
     this.items.forEach((item) => {
       item.className = "sortable-list__item";
       item.style.top = 0 + "px";
@@ -40,10 +40,10 @@ export default class SortableList {
     this.element.addEventListener("pointerdown", (event) => {
       event.preventDefault();
       let target = null;
-      // TODO: Нужны координаты всех span, которые можно грабать
-      // TODO: При переводе на эти координаты, менять весь li местами с target
-      // YES = TODO: При pointerUp добавлять классы и HTML target и удалять копию
 
+      if (event.target.closest("[data-delete-handle]")) {
+        event.target.closest(".sortable-list__item").remove();
+      }
       if (event.target.closest("[data-grab-handle]")) {
         target = event.target.closest(".sortable-list__item");
         target.ondragstart = () => false;
@@ -65,31 +65,61 @@ export default class SortableList {
         // ============================================================
         function onPointerMove(event) {
           moveAt(event.pageY);
-          let topOfParent = target.offsetParent.offsetTop;
-
           copyTarget.style.display = "none";
 
-          let elemBelow = document.elementFromPoint(
+          let elemUnderPointer = document.elementFromPoint(
             event.clientX,
             event.clientY
           );
 
           try {
-            if (!elemBelow) throw new Error("Ты за пределами экрана");
-            if (!elemBelow.closest(".sortable-list"))
-              throw new Error("вернись в контейнер");
-            if (!elemBelow.closest(".sortable-list__item"))
-              throw new Error("Нет нужного родителя");
+            if (!elemUnderPointer) throw new Error("Ты за пределами экрана");
+            if (elemUnderPointer === target)
+              throw new Error("Лишняя обработка");
+            if (elemUnderPointer === target.offsetParent)
+              throw new Error("offsetParent не считать");
+            if (!elemUnderPointer.closest(".sortable-list"))
+              throw new Error("Вернись в контейнер");
 
-            // TODO: Нужно прибавлять / убавлять координаты
-            // YES - TODO: вниз
-            // TODO: вверх
-            if (elemBelow !== target) {
-              elemBelow.style.top = parseInt(elemBelow.style.top) - 76 + "px";
-              target.style.top = parseInt(target.style.top) + 76 + "px";
+            const halfElemUnderPointer = elemUnderPointer.offsetHeight / 2;
+
+            const pointerCoordY = event.pageY - target.offsetParent.offsetTop;
+            const bottomCoordElemUnderPointer =
+              elemUnderPointer.offsetTop + elemUnderPointer.offsetHeight;
+
+            const bottomHalfElemUnderPointer =
+              pointerCoordY >
+                bottomCoordElemUnderPointer - halfElemUnderPointer &&
+              pointerCoordY < bottomCoordElemUnderPointer;
+
+            const topHalfElemUnderPointer =
+              pointerCoordY > elemUnderPointer.offsetTop &&
+              pointerCoordY < elemUnderPointer.offsetTop + halfElemUnderPointer;
+
+            let topCoordsElemUnderPointer = parseInt(
+              elemUnderPointer.style.top
+            );
+            let topCoordTargetElem = parseInt(target.style.top);
+
+            const margin =
+              parseInt(getComputedStyle(elemUnderPointer).marginTop) ?? 0;
+
+            const step = target.offsetHeight + margin;
+
+            if (bottomHalfElemUnderPointer) {
+              topCoordsElemUnderPointer += step;
+              topCoordTargetElem -= step;
             }
+
+            if (topHalfElemUnderPointer) {
+              topCoordsElemUnderPointer -= step;
+              topCoordTargetElem += step;
+            }
+
+            elemUnderPointer.style.top = topCoordsElemUnderPointer + "px";
+            target.style.top = topCoordTargetElem + "px";
           } catch (e) {
-            console.log(e.message);
+            // console.log(e.message);
           }
 
           copyTarget.style.display = "flex";
@@ -110,61 +140,3 @@ export default class SortableList {
     });
   }
 }
-/* 
-copyElem(className, innerHTML) {
-    const copyTarget = document.createElement("li");
-    copyTarget.className = className;
-    copyTarget.innerHTML = innerHTML;
-    copyTarget.style.position = "absolute";
-    // style.top работает до margin
-    copyTarget.style.margin = 0 + "px";
-    copyTarget.style.zIndex = 1000;
-    return copyTarget;
-  }
-
-  eventListeners() {
-    // отслеживаем нажатие
-
-    this.element.addEventListener("pointerdown", (event) => {
-      event.preventDefault();
-      // подготовил элемент
-      let target = null;
-      if (event.target.closest("[data-grab-handle]")) {
-        target = event.target.closest(".sortable-list__item");
-        const elemWidth = target.offsetWidth; // 778
-        const elemHeight = target.offsetHeight;
-
-        const copyTarget = this.copyElem(target.className, target.innerHTML);
-        target.classList.add("sortable-list__placeholder");
-        // TODO: Скрыть содержимое в оригинале, но так, чтобы потом его восстановить.
-        // либо в конце снова скопировать из копии
-        // либо сделать какой-то буфер, который будет временно хранить эти данные.
-        // TODO: оригинал должен следовать за копией в каждую ячейку
-        // TODO:
-
-        // перемещение в body, чтобы элемент не был внутри relative
-        document.body.append(copyTarget);
-
-        moveAt(event.pageY);
-        target.ondragstart = () => false;
-        function moveAt(pageY) {
-          copyTarget.style.width = elemWidth + "px";
-          // по одной вертикали
-          copyTarget.style.left = copyTarget.offsetLeft + "px";
-          copyTarget.style.top = pageY - elemHeight / 2 + "px";
-        }
-
-        function onPointerMove(event) {
-          moveAt(event.pageY);
-        }
-        // Перемещение по экрану
-        document.addEventListener("pointermove", onPointerMove);
-
-        copyTarget.onpointerup = function () {
-          document.removeEventListener("pointermove", onPointerMove);
-          copyTarget.onpointerup = null;
-        };
-      }
-    });
-  }
-*/
